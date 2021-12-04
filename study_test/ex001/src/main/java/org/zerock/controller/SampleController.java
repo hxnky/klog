b8d750e36e483887dfe395eb56e3362c58704e3c
hxnky <71997918+hxnky@users.noreply.github.com>
2021-12-04 16:52:28 +0900

    MVC 프로젝트 설정(예외처리, 컨틀롤러 등 테스트)

--- study_test/ex001/src/main/java/org/zerock/controller/SampleController.java
+++ study_test/ex001/src/main/java/org/zerock/controller/SampleController.java
@@ -1,11 +1,27 @@
 package org.zerock.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TOdoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -35,13 +51,107 @@ public class SampleController {
 
 		return "ex01";
 	}
-	
+
+	// 파라미터 받기
 	@GetMapping("/ex02")
 	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
 		log.info("name: " + name);
 		log.info("age: " + age);
-		
+
 		return "ex02";
+	}
+
+	// 파라미터 리스트로 받기
+	@GetMapping("/ex02List")
+	public String ex02List(@RequestParam("ids") ArrayList<String> ids) {
+		log.info("ids :" + ids);
+
+		return "ex02List";
+	}
+
+	// 파라미터 배열로 받기
+	@GetMapping("/ex02Array")
+	public String ex02Array(@RequestParam("ids") String[] ids) {
+
+		log.info("ids 배열 : " + ids);
+
+		return "ex02Array";
+	}
+
+	// 파라미터 리스트로 여러개 받기
+	@GetMapping("/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+		log.info("list dtos : " + list);
+
+		return "ex02Bean";
+	}
+
+	// InitBinder로 날짜 데이터 변환
+	// 이거 대신에 domain에서 바로 @DataTimeFormat 사용해도 됨
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dataFormat, false));
+	}
+
+	@GetMapping("/ex03")
+	public String ex03(TOdoDTO todo) {
+		log.info("todo : " + todo);
+
+		return "ex03";
+	}
+
+	// @ModelAttribute는 파라미터 타입과 관계없이 전달 가능
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+		log.info("dto : " + dto);
+		log.info("page : " + page);
+
+		return "/sample/ex04";
+	}
+
+	@GetMapping("/ex05")
+	public void ex05() {
+		log.info("exo5........");
+	}
+
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		log.info("/ex06");
+
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(10);
+		dto.setName("경");
+
+		return dto;
+	}
+
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07() {
+		log.info("/ex07...");
+
+		String msg = "{\"name\": \"홍길동\"}";
+
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-Type", "application/json;charset=UTF-8");
+
+		return new ResponseEntity<>(msg, header, HttpStatus.OK);
+	}
+	
+	// 파일 업로드 폼
+	@GetMapping("/exUpload")
+	public void exUpload() {
+		log.info("/exUpload.........");
+	}
+	
+	// 파일 업로드 시 파일 받음
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+		files.forEach(file -> {
+			log.info("========================");
+			log.info("name : " + file.getOriginalFilename());
+			log.info("size : " + file.getSize());
+		});
 	}
 
 }
---
+++ study_test/ex001/src/main/java/org/zerock/domain/TOdoDTO.java
@@ -0,0 +1,13 @@
+package org.zerock.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class TOdoDTO {
+
+	private String title;
+	private Date dueDate;
+	
+}
